03403a09e9c3968fed7d6e0ee873fee5df0c6e7b
ZZJ <zhaozjseu@163.com>
2019-11-25 18:45:32 +0800

    完蛋，应该在nginx打印log

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const redirectList = require('./redirect-config.json');
-const fs = require('fs')
-const morgan = require('morgan');
-const path = require('path');
 
 const app = express();
 const listenPort = 6599;
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-
-app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get(/.*/, function (req, res) {
   try{
