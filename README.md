1415caf710cd5cc7213fd232ab43e18edfac04e5
ZZJ <zhaozjseu@163.com>
2019-11-27 19:54:55 +0800

    feature : 匹配多个params

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rikumi-redirect
+
+配置文件redirect-config.json中的地址不以/结尾
+
+将http://auth.myseu.cn/<redirectName>/param1/param2...... (最后不以/结尾)
+
+跳转至<redirectUrl>/param1/param2/......?ticket=<ST-XXXXXXXXXXXXXX>
--- app.js
+++ app.js
@@ -5,22 +5,29 @@ const app = express();
 const listenPort = 6599;
 
 app.get(/.*/, function (req, res) {
-  try{
-    const redirectName = req.originalUrl.split('?')[0].slice(1);
-    const redirectUrl = redirectList[redirectName];
-    const ticket = req.query.ticket;
-    if (redirectUrl) {
+  try {
+    const redirectName = req.originalUrl.split('?')[0].split('/').slice(1)[0];
+    const params = req.originalUrl.split('?')[0].split('/').slice(2);
+    if (redirectList[redirectName]) {
+      // http://auth.myseu.cn/<redirectName>/param1/param2...... (最后不以/结尾)
+      // <redirectUrl>/param1/param2/......?ticket=<ST-XXXXXXXXXXXXXX>
+      let redirectUrl = redirectList[redirectName];
+      params.forEach(param => {
+        redirectUrl = redirectUrl + '/' + param
+      })
+      const ticket = req.query.ticket;
       console.log('重定向到:' + redirectUrl + '?ticket=' + ticket);
       res.redirect(redirectUrl + '?ticket=' + ticket);
-    }else{
+    } else {
       console.log('没有重定向目标');
       res.status(404).send('没有重定向目标')
     }
-  }catch(e){
+  } catch (e) {
+    console.log(e)
     console.log('重定向错误，请检查')
     res.status(404).send('重定向错误，请检查')
   }
-  
+
 });
 
 app.listen(listenPort, function () {
